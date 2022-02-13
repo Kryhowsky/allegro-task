a7b407372d1c7bead4436f9f750adb78b69559cb
Kryhowsky <piter71-1997@go2.pl>
2022-02-13 21:21:12 +0100

    Initial commit

---
+++ src/test/java/com/kryhowsky/allegrotask/AllegroTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kryhowsky.allegrotask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllegroTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
